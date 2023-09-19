ede914143a1d69f7db518c576e7700b3dd66f59e
Sean Nichols <78358189+OGreatHippo@users.noreply.github.com>
2023-09-19 14:47:43 +0100

    initial commit

---
+++ CHARACTER_SHEET/Source/CHARACTER_SHEET/CHARACTER_SHEET.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CHARACTER_SHEET.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CHARACTER_SHEET, "CHARACTER_SHEET" );
