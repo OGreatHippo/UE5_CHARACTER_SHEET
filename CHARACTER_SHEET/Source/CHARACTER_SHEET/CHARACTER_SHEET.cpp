e3af8168855ca5f207fa9af3a5c6146623cdb413
Sean Nichols <78358189+OGreatHippo@users.noreply.github.com>
2023-09-19 15:08:20 +0100

    why is ue5 so bad

--- CHARACTER_SHEET/Source/CHARACTER_SHEET/CHARACTER_SHEET.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CHARACTER_SHEET.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CHARACTER_SHEET, "CHARACTER_SHEET" );
